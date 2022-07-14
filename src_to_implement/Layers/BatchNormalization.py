Batch normalization fixed - test cases for BN is working

src_to_implement/Layers/BatchNormalization.py
@@ -1,10 +1,18 @@
 import numpy as np
 from .Base import BaseLayer
+from .Helpers import compute_bn_gradients
 
 
 class BatchNormalization(BaseLayer):
     def __init__(self, channels):
         # super().__init__()
+        self._gradient_bias = None
+        self._gradient_weights = None
+        self._bias_optimizer = None
+        self.g_b = None
+        self.g_w = None
+        self.variance = 1
+        self.mean = 0
         self.tensor_shape = None
         self.testing_phase = False
         self.trainable = True
@@ -15,52 +23,128 @@ class BatchNormalization(BaseLayer):
         self.initialize(channels)
 
         self._optimizer = None
+        self.moving_mean = 0
+        self.moving_variance = 0
+        self.input_tensor = None
+        self.normalized_input_tensor = None
 
     def initialize(self, shape):
-        self.weights = np.ones((shape))
-        self.bias = np.zeros((shape))
+        self.weights = np.ones(shape)
+        self.bias = np.zeros(shape)
 
-
-    def forward(self, input_tensor):
+    def forward(self, input_tensor, alpha=0.8):
         self.tensor_shape = input_tensor.shape
+        self.input_tensor = input_tensor
         # moving average provides better estimate of the true mean and variance compared to individual mini-batch
         # https://stackoverflow.com/questions/60460338/does-batchnormalization-use-moving-average-across-batches-or-only-per-batch-and
+        y_bar = None
+        eps = np.finfo(float).eps
+        if len(input_tensor.shape) == 4:
+            # if Convolution layer gives output with multiple dimensions ( batch, no. of kernel, height of image, width of image)
+            self.mean = np.mean(input_tensor, axis=(0, 2, 3))
+            self.variance = np.var(input_tensor, axis=(0, 2, 3))
+            no_of_kernels = input_tensor.shape[1]
+            if not self.testing_phase:
+                # Training Phase
+                mean = np.mean(input_tensor, axis=(0, 2, 3))
+                variance = np.var(input_tensor, axis=(0, 2, 3))
+                self.moving_mean = alpha * self.mean + (1-alpha) * mean
+                self.moving_variance = alpha * self.variance + (1-alpha)*variance
+                self.mean = mean
+                self.variance = variance
+                self.normalized_input_tensor = (input_tensor - self.mean.reshape((1, no_of_kernels, 1, 1))) / np.sqrt(self.variance.reshape((1, no_of_kernels, 1, 1)) + eps)
+                y_bar = self.weights.reshape((1, no_of_kernels, 1, 1)) * self.normalized_input_tensor + self.bias.reshape((1, no_of_kernels, 1, 1))
+            else:
+                # Testing Phase
+                self.normalized_input_tensor = (input_tensor - self.moving_mean.reshape((1, no_of_kernels, 1, 1))) / np.sqrt(self.moving_variance.reshape((1, no_of_kernels, 1, 1)) + eps)
+                y_bar = self.weights.reshape((1, no_of_kernels, 1, 1)) * self.normalized_input_tensor + self.bias.reshape((1, no_of_kernels, 1, 1))
 
-        # In training we need to calculate mini batch mean in order to normalize the batch
-        # In the inference we just apply pre-calculated mini batch statistics
-        if not self.testing_phase:
-            # during training
-
-            # running_mean = momentum * running_mean + (1 - momentum) * sample_mean
-            # running_var = momentum * running_var + (1 - momentum) * sample_var
-            eps = np.finfo(float).eps
-            tensor = self.reformat(input_tensor)
-            mean = np.mean(tensor, axis=0)
-            var = np.var(tensor, axis=0)
-            normalized_input_tensor = (input_tensor - mean)/np.sqrt(var + eps)
-            y_bar = self.weights * normalized_input_tensor + self.bias
-            return y_bar
         else:
-            return
+            # if only batch number and number of kernels comes out
+            self.mean = np.mean(input_tensor, axis=0)
+            self.variance = np.var(input_tensor, axis=0)
+            if not self.testing_phase:
+                # Training Phase
+                mean = np.mean(input_tensor, axis=0)
+                variance = np.var(input_tensor, axis=0)
+                self.moving_mean = alpha * self.mean + (1 - alpha) * mean
+                self.moving_variance = alpha * self.variance + (1 - alpha) * variance
+                self.mean = mean
+                self.variance = variance
+                self.normalized_input_tensor = (input_tensor - self.mean) / np.sqrt(self.variance + eps)
+                y_bar = self.weights * self.normalized_input_tensor + self.bias
+            else:
+                # Testing Phase
+                self.normalized_input_tensor = (input_tensor - self.moving_mean) / np.sqrt(self.moving_variance + eps)
+                y_bar = self.weights * self.normalized_input_tensor + self.bias
+        return y_bar
 
     def backward(self, error_tensor):
-        return
+        eps = np.finfo(float).eps
+        if len(error_tensor.shape) == 4:
+            output = compute_bn_gradients(error_tensor = self.reformat(error_tensor),
+                                          input_tensor=self.reformat(self.input_tensor),
+                                          weights=self.weights,
+                                          mean=self.mean,
+                                          var=self.variance)
+            output = self.reformat(output)
+            self.gradient_weights = np.sum(error_tensor * self.normalized_input_tensor, axis=(0, 2, 3))
+            self.gradient_bias = np.sum(error_tensor, axis=(0, 2, 3))
+
+        else:
+            output = compute_bn_gradients(error_tensor = self.reformat(error_tensor),
+                                          input_tensor=self.reformat(self.input_tensor),
+                                          weights=self.weights,
+                                          mean=self.mean,
+                                          var=self.variance)
+            self.gradient_weights = np.sum(error_tensor * self.normalized_input_tensor, axis=0)
+            self.gradient_bias = np.sum(error_tensor, axis=0)
+        if self._optimizer:
+            self.weights = self._optimizer.calculate_update(self.weights, self._gradient_weights)
+            self.bias = self._optimizer.calculate_update(self.bias, self._gradient_bias)
+        return output
 
     def reformat(self, tensor):
-
         if len(tensor.shape) > 2:
-
-            tensor = tensor.reshape(self.tensor_shape[0], self.tensor_shape[1], np.prod(self.tensor_shape[2:]))
+            # this will run when there are 4 dimensions
+            tensor_shape = tensor.shape
+            tensor = tensor.reshape(tensor_shape[0], tensor_shape[1], np.prod(tensor_shape[2:]))
             tensor = np.swapaxes(tensor, 1, tensor.ndim-1)
             # reshape to B.M.N x H
-            tensor = tensor.reshape(-1, self.tensor_shape[1])
+            tensor = tensor.reshape(-1, tensor_shape[1])
             return tensor
         else:
-            # if ka reverse will be here
-            tensor = tensor.reshape(self.tensor_shape[0], -1, self.tensor_shape[1])
+            # reverse of "if" is here
+            tensor_shape = self.input_tensor.shape
+            tensor = tensor.reshape(tensor_shape[0], -1, tensor_shape[1])
             tensor = np.swapaxes(tensor, 1, tensor.ndim-1)
-            tensor = tensor.reshape(self.tensor_shape[0], self.tensor_shape[1], *(self.tensor_shape[2:]))
+            tensor = tensor.reshape(tensor_shape[0], tensor_shape[1], *(tensor_shape[2:]))
             return tensor
+
+    @property
+    def gradient_weights(self):
+        return self._gradient_weights
+
+    @gradient_weights.setter
+    def gradient_weights(self, value):
+        self._gradient_weights = value
+
+    @gradient_weights.deleter
+    def gradient_weights(self):
+        del self._gradient_weights
+
+    @property
+    def gradient_bias(self):
+        return self._gradient_bias
+
+    @gradient_bias.setter
+    def gradient_bias(self, value):
+        self._gradient_bias = value
+
+    @gradient_bias.deleter
+    def gradient_bias(self):
+        del self._gradient_bias
+
     @property
     def optimizer(self):
         return self._optimizer
@@ -69,6 +153,18 @@ class BatchNormalization(BaseLayer):
     def optimizer(self, value):
         self._optimizer = value
 
-    @optimizer.getter
+    @optimizer.deleter
     def optimizer(self):
-        return self._optimizer
+        del self._optimizer
+
+    @property
+    def bias_optimizer(self):
+        return self._bias_optimizer
+
+    @bias_optimizer.setter
+    def bias_optimizer(self, value):
+        self._bias_optimizer = value
+
+    @bias_optimizer.deleter
+    def bias_optimizer(self):
+        del self._bias_optimizer

src_to_implement/Optimization/Constraints.py
@@ -1,8 +1,8 @@
+import numpy as np
 
 
 class L2_Regularizer:
     def __init__(self, alpha):
-        self.weights = None
         self.alpha = alpha
         pass
 
@@ -10,20 +10,17 @@ class L2_Regularizer:
         """
             Calculate sub-gradients on the weights needed for the optimization
         """
-        self.weights = weights
-
-        return
+        return weights * self.alpha
 
     def norm(self, weights):
         """
             Calculate the norm enhanced loss
         """
-        return
+        return self.alpha * np.sqrt((np.square(weights).sum()))
 
 
 class L1_Regularizer:
     def __init__(self, alpha):
-        self.weights = None
         self.alpha = alpha
         pass
 
@@ -31,12 +28,12 @@ class L1_Regularizer:
         """
             Calculate sub-gradients on the weights needed for the optimization
         """
-        self.weights = weights
-
-        return
+        # because derivative of weights is 0 hence we multiplied it with lambda directly
+        return self.alpha * np.sign(weights)
 
     def norm(self, weights):
         """
             Calculate the norm enhanced loss
         """
-        return
+        # Only considering the absolute value
+        return self.alpha * np.abs(weights).sum()

src_to_implement/Optimization/Optimizers.py
@@ -21,6 +21,9 @@ class SgdWithMomentum(Optimizer):
 
     def calculate_update(self, weight_tensor, gradient_tensor):
         self.vk = self.momentum_rate * self.vk - self.learning_rate * gradient_tensor
+        if self.regularizer:
+            regularized_gradient = self.regularizer.calculate_gradient(weight_tensor)
+            weight_tensor = weight_tensor - self.learning_rate * regularized_gradient
         weight_tensor = weight_tensor + self.vk
         return weight_tensor
 
@@ -41,16 +44,22 @@ class Adam(Optimizer):
         vk_vector = self.vk/(1-np.power(self.mu, self.pow))
         rk_vector = self.rk / (1 - np.power(self.rho, self.pow))
         self.pow += 1
+        if self.regularizer:
+            regularized_gradient = self.regularizer.calculate_gradient(weight_tensor)
+            weight_tensor = weight_tensor - self.learning_rate * regularized_gradient
         weight_tensor -= self.learning_rate * (vk_vector/(np.sqrt(rk_vector) + eps))
         return weight_tensor
 
 
 class Sgd(Optimizer):
     def __init__(self, learning_rate) -> None:
+        super().__init__()
         self.learning_rate = learning_rate
         pass
 
     def calculate_update(self, weight_tensor, gradient_tensor):
-        weight_tensor -= gradient_tensor * self.learning_rate
-        return weight_tensor
+        if self.regularizer:
+            regularized_gradient = self.regularizer.calculate_gradient(weight_tensor)
+            weight_tensor -= regularized_gradient * self.learning_rate
+        return weight_tensor - self.learning_rate * gradient_tensor
 